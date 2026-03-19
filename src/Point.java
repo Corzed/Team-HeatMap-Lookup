6869dcff846e86027ff0a476b9f108ef140a6d85
MthreeJohnnyK <s-jzhong@lwsd.org>
2026-03-18 18:41:08 -0700

    reweighted distance calc

--- src/Point.java
+++ src/Point.java
@@ -15,7 +15,7 @@ public class Point {
         return new Point(y, -x);
     }
     public double distanceTo(Point other){
-        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow(this.y - other.y, 2));
+        return Math.sqrt(Math.pow(this.x - other.x, 2) + Math.pow((this.y - other.y) / 2, 2)); //Y is divided by 2 to account for the fact that 1 units change in y corresponds to a change of 0.5 units in x, so we need to divide the difference in y-coordinates by 2 before squaring it and adding it to the squared difference in x-coordinates.
     }
     public String toString(){
         return "(" + x + ", " + y + ")";
